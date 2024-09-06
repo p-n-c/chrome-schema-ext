26fbc9255fe7d8a2a339b94aa3b7562948c49197
Jean-Philippe Drecourt <jpdrecourt@gmail.com>
2024-09-06 16:22:43 +0100

    New directory structure

--- popup.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('generate-schema').addEventListener('click', () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'generateSchema' })
-  })
-})
